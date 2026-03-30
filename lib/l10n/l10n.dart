83b6df0418dac42a457a931a6a63602cdaaf6a5c
LITES <liunlong@gmail.com>
2026-03-31 05:01:34 +0800

    test

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -4918,6 +4918,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalizations> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ru'),
       Locale.fromSubtags(languageCode: 'zh', countryCode: 'CN'),
       Locale.fromSubtags(languageCode: 'zh', countryCode: 'TC'),
     ];
